8dab6373063cc679a868b04e3f510f6db99bd85c
anshbest <anshulmnc123@gmail.com>
2020-08-09 07:43:07 +0530

    doublet combination and adding percentage for same

--- coincombinator.sh
+++ coincombinator.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-declare -A tossdict
-tossdict[H]=0
-tossdict[T]=0
+declare -A Ischoicedict
+Ischoicedict[H]=0
+Ischoicedict[T]=0
+Ischoicedict[HH]=0
+Ischoicedict[HT]=0
+Ischoicedict[TH]=0
+Ischoicedict[TT]=0
 
-for (( i=0 ; i<20 ; i++ ))
+for (( i=0 ; i<10 ; i++ ))
 do
-	Ischoice=$((RANDOM%2))
-	if [ $Ischoice -eq 0 ]
+	isChoice=$((RANDOM%2))
+	if [ $isChoice -eq 0 ]
 	then
-		((tossdict[H]++))
+		((Ischoicedict[H]++))
 	else
-		((tossdict[T]++))
+		((Ischoicedict[T]++))
 	fi
+	isChoice2=$((RANDOM%2))
+	case $isChoice$isChoice2 in
+		00)
+			((Ischoicedict[HH]++))
+			;;
+		01)
+			((Ischoicedict[HT]++))
+			;;
+		10)
+			((Ischoicedict[TH]++))
+			;;
+		11)
+			((Ischoicedict[TT]++))
+			;;
+	esac
 done
-echo "H prcnt  is $((${tossdict[H]}*100/20))%"
-echo " T prcnt is $((${tossdict[T]}*100/20))%"
+echo "percnt of H is $((${Ischoicedict[H]}*100/10))%"
+echo "percnt of T is $((${Ischoicedict[T]}*100/10))%"
+echo "percnt of HH is $((${Ischoicedict[HH]}*100/10))%"
+echo "percnt of H T is $((${Ischoicedict[HT]}*100/10))%"
+echo "percnt of T H is $((${Ischoicedict[TH]}*100/10))%"
+echo "percnt of T T is $((${Ischoicedict[TT]}*100/10))%"
