ec662f304921e79ef40d300cd62aaab494eb9ce7
anshbest <anshulmnc123@gmail.com>
2020-08-09 08:04:31 +0530

    Singlet Doublet and Triplet with winning percentage

--- coincombinator.sh
+++ coincombinator.sh
@@ -1,52 +1,106 @@
 #!/bin/bash
 
-declare -A Ischoicedict
-Ischoicedict[HHH]=0
-Ischoicedict[HHT]=0
-Ischoicedict[HTH]=0
-Ischoicedict[THH]=0
-Ischoicedict[HTT]=0
-Ischoicedict[THT]=0
-Ischoicedict[TTH]=0
-Ischoicedict[TTT]=0
+declare -a counttossvalue
+declare -a counttosskey
+declare -A tossdic
+tossdic[HHH]=0
+tossdic[HHT]=0
+tossdic[HTH]=0
+tossdic[THH]=0
+tossdic[HTT]=0
+tossdic[THT]=0
+tossdic[TTH]=0
+tossdic[TTT]=0
+tossdic[HH]=0
+tossdic[HT]=0
+tossdic[TH]=0
+tossdic[TT]=0
+tossdic[H]=0
+tossdic[T]=0
 
-for (( i=0 ; i<20 ; i++ ))
-do
-	isChoice=$((RANDOM%2))
-	isChoice2=$((RANDOM%2))
-	isChoice3=$((RANDOM%2))
-	case $isChoice$isChoice2$isChoice3 in
-		000)
-			((Ischoicedict[HHH]++))
-			;;
-		001)
-			((Ischoicedict[HHT]++))
-			;;
-		010)
-			((Ischoicedict[HTH]++))
-			;;
-		100)
-			((Ischoicedict[THH]++))
-			;;
-		011)
-			((Ischoicedict[HTT]++))
-			;;
-		101)
-			((Ischoicedict[THT]++))
-			;;
-		110)
-			((Ischoicedict[TTH]++))
-			;;
-		111)
-			((Ischoicedict[TTT]++))
-			;;
-	esac
+for  (( i=0 ; i<40 ; i++ ))
+ do
+        whatchoice=$((RANDOM%14))
+        case $whatchoice in
+                0)
+                        ((tossdic[HHH]++))
+                        ;;
+                1)
+                        ((tossdic[HHT]++))
+                        ;;
+                2)
+                        ((tossdic[HTH]++))
+                        ;;
+                3)
+                        ((tossdic[THH]++))
+                        ;;
+                4)
+                        ((tossdic[HTT]++))
+                        ;;
+                5)
+                        ((tossdic[THT]++))
+                        ;;
+                6)
+                        ((tossdic[TTH]++))
+                        ;;
+                7)
+                        ((tossdic[TTT]++))
+                        ;;
+                8)
+                        ((tossdic[HH]++))
+                        ;;
+                9)
+                        ((tossdic[HT]++))
+                        ;;
+                10)
+                        ((tossdic[TH]++))
+                        ;;
+                11)
+                        ((tossdic[TT]++))
+                        ;;
+                12)
+                        ((tossdic[H]++))
+                        ;;
+                13)
+                        ((tossdic[T]++))
+                        ;;
+        esac
 done
-echo "percnt of HHH is $((${Ischoicedict[HHH]}*100/20))%"
-echo "percnt of HHT is $((${Ischoicedict[HHT]}*100/20))%"
-echo "percnt of HTH is $((${Ischoicedict[HTH]}*100/20))%"
-echo "percnt of THH is $((${Ischoicedict[THH]}*100/20))%"
-echo "percnt of HTT is $((${Ischoicedict[HTT]}*100/20))%"
-echo "percnt of THT is $((${Ischoicedict[THT]}*100/20))%"
-echo "percnt of TTH is $((${Ischoicedict[TTH]}*100/20))%"
-echo "percnt of TTT is $((${Ischoicedict[TTT]}*100/20))%"
+i=0
+for value in `echo ${tossdic[@]}`
+do
+        counttossvalue[$i]=$value
+        ((i++))
+done
+i=0
+for  key in `echo ${!tossdic[@]}`
+do
+        counttosskey[$i]=$key
+        ((i++))
+done
+comparemax=0
+comparemaxindex=0
+
+for  (( index=0 ; index<14 ; index++ ))
+do
+        if [ ${counttossvalue[$index]} -gt $comparemax ]
+        then
+                comparemax=${counttossvalue[index]}
+                comparemaxindex=$index
+        fi
+done
+echo "H has count%" `awk "BEGIN{print ${tossdic[H]}/40*100}"` "%"
+echo "T has count%" `awk "BEGIN{print ${tossdic[T]}/40*100}"` "%"
+echo "H H has count%" `awk "BEGIN{print ${tossdic[HH]}/40*100}"` "%"
+echo "H T has count%" `awk "BEGIN{print ${tossdic[HT]}/40*100}"` "%"
+echo "T H has count%" `awk "BEGIN{print ${tossdic[TH]}/40*100}"` "%"
+echo "T T has count%" `awk "BEGIN{print ${tossdic[TT]}/40*100}"` "%"
+echo "H H H has count% " `awk "BEGIN{print ${tossdic[HHH]}/40*100}"` "%"
+echo "H H T has count%" `awk "BEGIN{print ${tossdic[HHT]}/40*100}"` "%"
+echo "H T H has count%" `awk "BEGIN{print ${tossdic[HTH]}/40*100}"` "%"
+echo "T H H has count%" `awk "BEGIN{print ${tossdic[THH]}/40*100}"` "%"
+echo "H T T has count%" `awk "BEGIN{print ${tossdic[HTT]}/40*100}"` "%"
+echo "T H T has count%" `awk "BEGIN{print ${tossdic[THT]}/40*100}"` "%"
+echo "T T H has count%" `awk "BEGIN{print ${tossdic[TTH]}/40*100}"` "%"
+echo "T T T has count%" `awk "BEGIN{print ${tossdic[TTT]}/40*100}"` "%"
+echo "The winner casehere is ${counttosskey[$comparemaxindex]} and it count% is" `awk "BEGIN{print ${counttossvalue[$comparemaxindex]}*100/40}"` "%"
