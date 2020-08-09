676853afffee995235598124b42f2371c719842e
anshbest <anshulmnc123@gmail.com>
2020-08-09 07:38:50 +0530

    percenatage add to previous problem

--- coincombinator.sh
+++ coincombinator.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-Ischoice=$((RANDOM%2))
-if [ $Ischoice -eq 0 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A tossdict
+tossdict[H]=0
+tossdict[T]=0
 
+for (( i=0 ; i<20 ; i++ ))
+do
+	Ischoice=$((RANDOM%2))
+	if [ $Ischoice -eq 0 ]
+	then
+		((tossdict[H]++))
+	else
+		((tossdict[T]++))
+	fi
+done
+echo "H prcnt  is $((${tossdict[H]}*100/20))%"
+echo " T prcnt is $((${tossdict[T]}*100/20))%"
